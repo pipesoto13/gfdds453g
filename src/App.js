8c2dd19cdb98a32f63e0ac031bb7ad5f61aba6d8
Felipe Soto C <pipesoto13@gmail.com>
2020-10-29 22:10:58 -0500

    add comments

--- src/App.js
+++ src/App.js
@@ -49,3 +49,41 @@ export const NotFound = (props) => {
     <h1>Pagina no encontrada</h1>
   )
 }
+
+/* 
+Nuestra solucion es la siguiente:
+
+En el archivo index.js envolvemos el App en el componente Router de react-router:
+
+<Router>
+  <App />
+</Router>
+Modificamos App.js para que quede de la siguiente manera:
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <nav>
+          <Link to='/page1'>Page 1</Link>
+          <Link to='/page2'>Page 2</Link>
+        </nav>
+
+        <Switch>
+          <Redirect exact from="/" to="/page1" />
+          <Route path="/page1" component={Page1} />
+          <Route path="/page2" component={Page2} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </div>
+    )
+  }
+}
+Primero utilizamos el componente Link de React Router para crear los vínculos a las rutas.
+
+Luego, utilizamos los componentes Switch, Redirect y Route para definir qué componente que se va a renderizar según la ruta en la que se encuentre.
+
+Switch se utiliza para que renderice sólo una ruta a la vez. Si se omite, y la ruta es /page1, se mostraría el componente Page1 y, debajo, NotFound.
+
+Redirect se utiliza para que cuando ingresen a la raíz (/) redireccione a /page1.
+ */
